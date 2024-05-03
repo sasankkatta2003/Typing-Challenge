fa858ce054a399fb5cc7ee31d9c00c893e3fdfac
Sasank Katta <sasankkatta2003@gmail.com>
2024-05-03 16:42:25 +0530

    Typing Challenge

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const typeArea = document.querySelector(".typingArea");
 const btn = document.querySelector(".btn");
 
 const words = [
-  "A day in the life of programmer",
-  "What is JavaScript?",
-  "What is React?",
-  "What is Programming Language?",
-  "What's your name?",
-  "Where are you from?",
-  "This is just random word",
-  "What is Remix.js?",
-  "New Technologies",
-  "Is programming hard?",
-  "Why do you wanna become a programmer?",
-  "Which programming language you like the most?",
-  "What is Golang? and why do you wanna learn it?",
-  "What is CSS",
+  "Note", "right", "number", "four", "went", "earth", "umbrella", "spell", "leave", "very",
+"Don't", "leave", "the", "dog", "away", "from", "the", "home", "its", "dangerous",
+"need", "on", "cross", "three", "such", "work", "low", "part", "to", "ship", "specially",
+"among", "live", "during", "often", "sound", "spell", "that", "five", "govern", "said", "that",
+"Do", "words", "not", "random", "expect", "the", "those", "to", "type", "are",
+"of", "object", "laugh", "people", "rock", "get", "king", "clear", "time", "man", "eventually",
+"give", "and", "keyboard", "sunshine", "done", "eye", "keep", "main", "sound",
+"everything", "south", "playing", "cross", "were", "came", "men", "his", "rock", "stock",
+"show", "young", "good", "new", "technologies", "which", "common", "must", "would",
+"stand", "hunderd", "minute", "but", "hour", "test", "over", "no", "stop",
+"remember", "want", "still", "eight", "late", "her", "see", "wait", "halfcarry", "too",
+"air", "ship", "four", "sound", "noun", "little", "dog", "that", "were", "perhaps",
+"mark", "stand", "now", "cross", "step", "said", "cold", "take", "since", "that", "hold",
+"front", "now", "feel", "at", "strong", "island", "rest", "man", "want", "map", "to", "travel",
 ];
+
+
+main.textContent="Ready to type?";
+typeArea.innerHTML = `Type here...`;
+typeArea.disabled = true;
+
 
 const game = {
   start: 0,
@@ -38,10 +44,23 @@ btn.addEventListener("click", () => {
   }
 });
 
+typeArea.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    typeArea.disabled = true;
+    main.style.borderColor = "white";
+    end();
+  }
+});
+
 function play() {
-  let randText = Math.floor(Math.random() * words.length);
-  main.textContent = words[randText];
-  game.arrText = words[randText];
+  const selectedWords = [];
+for (let i = 0; i < 15; i++) {
+    const randIndex = Math.floor(Math.random() * words.length);
+    selectedWords.push(words[randIndex]);
+}
+const selectedText = selectedWords.join("\n");
+  main.textContent = selectedText;
+  game.arrText = selectedText;
   main.style.borderColor = "#c8c8c8";
   btn.textContent = "Done";
   const duration = new Date();
@@ -55,13 +74,14 @@ function end() {
   game.user = typeArea.value;
   const correct = results();
   main.style.borderColor = "white";
-  main.innerHTML = `Time: ${totalTime} Score: ${correct.score} out of ${correct.total}`;
+  const val=((correct.score/correct.total)*100).toFixed(2);
+  main.innerHTML = `Time: ${totalTime} Accuracy: ${val}% <br><span class="click-start">Click Start to Play again</span>`;
   btn.textContent = "Start";
 }
 
 function results() {
-  let valueOne = game.arrText.split(" ");
-  let valueTwo = game.user.split(" ");
+  const valueOne = game.arrText.trim().split(/\s+/);
+    const valueTwo = game.user.trim().split(/\s+/);
   let score = 0;
   valueOne.forEach((word, idx) => {
     if (word === valueTwo[idx]) {
